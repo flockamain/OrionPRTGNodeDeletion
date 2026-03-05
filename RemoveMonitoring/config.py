2e62ecc0f5a3435b282a627755dd8efa4c9e31f7
Alex Wisch <alexjwisch@gmail.com>
2026-03-05 10:55:18 -0600

    Create config.py with server credentials
    example of what config.py should look like

---
+++ RemoveMonitoring/config.py
@@ -0,0 +1,8 @@
+PRTG_SERVER = ''    # e.g., https://prtg.company.net
+ORION_SERVER = ''  # e.g., your-orion-server.company.net -- https:// is auto added, so dont include here
+
+PRTG_USER = ''
+PRTG_PASS = '' #passhash or password
+
+ORION_USER = ''
+ORION_PASS = ''
